3da723e8d4d8f45cd92b8ab7b29cdcf4ac021667
Abdullah Ghani <35004752+Abdullah-Ghani@users.noreply.github.com>
2018-09-23 21:24:39 -0400

    Add files via upload

---
+++ Problem 1/Main.java
@@ -0,0 +1,29 @@
+
+
+
+public class Main {
+
+    public static void main(String[]args){
+        ArbitraryQueue list =new ArbitraryQueue();
+
+        list.Pop();
+        list.Dequeue();
+        list.Push(3);
+        list.Pop();
+        list.Pop();
+        list.Push("hello");
+        list.Push(4);
+        list.Push(6);
+        list.Push(90);
+        list.Enqueue(54);
+        list.Enqueue(43);
+        list.printer();
+        list.Pop();
+
+        list.Dequeue();
+        list.Traverse(6);
+        list.printer();
+
+
+    }
+}
